aeb178b945717e5c6d596ab65ed3e9183a78b148
Korribu <bb.belari@gmail.com>
2023-03-03 16:11:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   version      = '0.0.3',
   description  = 'chroot installer for Red Hat derivatives',
   #scripts      = glob("bin/*"), # yumbootstrap should go to sbin, not to bin
-  packages      = ['lib', 'lib/yumbootstrap'],
+  packages      = ['lib'],
   #packages     = find_packages("lib"),
   package_dir  = { "": "lib" },
 )
