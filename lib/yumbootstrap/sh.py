83377bdb183b060272cb100b259c5ee942cc0fe9
Korribu <bb.belari@gmail.com>
2023-03-06 13:58:12 +0300

    Add files via upload

--- lib/yumbootstrap/sh.py
+++ lib/yumbootstrap/sh.py
@@ -2,7 +2,6 @@
 
 import os
 import subprocess
-from builtins import BaseException
 from yumbootstrap.exceptions import YBError
 
 READ  = object() # read from
@@ -31,7 +30,7 @@ class OutPipe:
   def write(self, data):
     try:
       return self._proc.stdin.write(data)
-    except IOError as e:
+    except IOError:
       self.close()
       # close() probably already raised an error, but if the command did
       # exit(0), let's die
@@ -40,19 +39,19 @@ class OutPipe:
   def sync(self):
     try:
       return self._proc.stdin.sync()
-    except IOError as e:
+    except IOError:
       self.close()
       # close() probably already raised an error, but if the command did
       # exit(0), let's die
       raise YBError('"%s" exited unexpectedly', self._cmd, exit = 1)
 
-   def close(self):
+  def close(self):
     proc = self._proc
     self._proc = None
     try:
       proc.communicate()
       check_error(self._cmd, proc.returncode)
-    except IOError as e:
+    except IOError:
       # it would be weird if I/O error happened on close(), but it could be
       # flushing buffers or something
       raise YBError('"%s" exited unexpectedly', self._cmd, exit = 1)
